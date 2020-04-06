757a2d3134d6ab8742c3a14751742d83f2b976d6
Kieran Connell <kieran@kconnell.co.uk>
2020-04-06 16:07:52 +0100

    Updated palette stream.
    Replaced green with cyan in boring tunnel.
    Updated VGM to Dave's latest test.

--- make-assets.bat
+++ make-assets.bat
@@ -4,5 +4,5 @@ echo Building ASSETS...
 python bin/png2bbc.py --quiet -o build/logo_mode1.bin --palette 0436 ./data/BeeBShifters.png 1
 bin\exomizer.exe level -M256 build/logo_mode1.bin@0x4E00 -o build/logo_mode1.exo
 python ..\vgm-packer\vgmpacker.py "data/Twitching Flannels.vgm" -o build/intro_theme.vgc
-python ..\vgm-packer\vgmpacker.py data/davetest3.vgm -o build/main_theme.vgc
+python ..\vgm-packer\vgmpacker.py data/STNICCC_BBC_Rhino_05_combined.vgm -o build/main_theme.vgc
 python ..\vgm-packer\vgmpacker.py "data/Torment 24h.vgm" -o build/outro_theme.vgc
