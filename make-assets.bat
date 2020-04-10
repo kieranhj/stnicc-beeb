c5ad5d740857d7ca6da8a07e8e0e927e2cc4e6b2
Kieran Connell <kieran@kconnell.co.uk>
2020-04-10 08:23:29 +0100

    Final main theme from Rhino.
    It's a beast!

--- make-assets.bat
+++ make-assets.bat
@@ -4,5 +4,5 @@ echo Building ASSETS...
 python bin/png2bbc.py --quiet -o build/logo_mode1.bin --palette 0436 ./data/BeeBShifters.png 1
 bin\exomizer.exe level -M256 build/logo_mode1.bin@0x4E00 -o build/logo_mode1.exo
 python ..\vgm-packer\vgmpacker.py "data/intro_test.vgm" -o build/intro_theme.vgc
-python ..\vgm-packer\vgmpacker.py data/STNICCC_BBC_Rhino_06_combined.vgm -o build/main_theme.vgc
+python ..\vgm-packer\vgmpacker.py "data/main_test.vgm" -o build/main_theme.vgc
 python ..\vgm-packer\vgmpacker.py "data/outro_test.vgm" -o build/outro_theme.vgc
